af79582e2c600ec4d463d4749354f34343c86f99
emilykaroliny2006 <129808025+emilykaroliny2006@users.noreply.github.com>
2024-07-31 20:11:33 -0300

    j6

---
+++ script.js
@@ -0,0 +1,7 @@
+import { aleatorio, nome } from './aleatorio.js';
+import { perguntas } from './perguntas.js';
+
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const textoResultado = document.querySelector(".texto-resultado")
